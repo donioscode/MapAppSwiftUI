60692dc4276fe3cb363679c72e37e5641a779939
donioscode <doniyornormuxammedov@gmail.com>
2024-06-24 18:57:57 +0500

    LocationsMapAnnotionView added to pinned location

---
+++ MapAppSwiftUI/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationMapAnnotationView.swift
+//  MapAppSwiftUI
+//
+//  Created by doniyor normuxammedov on 24/06/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationMapAnnotationView()
+}
