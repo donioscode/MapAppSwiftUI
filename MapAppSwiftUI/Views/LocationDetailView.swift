b1893a02cce2277a5b0a1ab3a0d15e23b2c1bf07
donioscode <doniyornormuxammedov@gmail.com>
2024-06-26 09:46:42 +0500

    LocationDetailView added to Detail

---
+++ MapAppSwiftUI/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapAppSwiftUI
+//
+//  Created by doniyor normuxammedov on 24/06/24.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
